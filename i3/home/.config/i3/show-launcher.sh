f90201303329b4339c1810a7f8af03ae86dc3802
noodlefighter <noodlefighter@gmail.com>
2021-05-18 15:30:45 +0800

    i3

--- i3/home/.config/i3/show-launcher.sh
+++ i3/home/.config/i3/show-launcher.sh
@@ -19,11 +19,7 @@ if ! [ $? = 0 ]; then
 fi
 
 rofi -dpi 1 -show combi \
-   -combi-modi run,drun -location 1 -width 100 -lines 10 -columns 1 \
-   -separator-style none -font "mono 10" -bw 0 \
+   -combi-modi run,drun,window -width 30 -lines 10 -columns 1 \
+   -separator-style none -font "mono 12" -bw 0 \
    -show-icons \
-   -color-window "#222222, #222222, #b1b4b3" \
-   -color-normal "#222222, #b1b4b3, #222222, #005577, #b1b4b3" \
-   -color-active "#222222, #b1b4b3, #222222, #007763, #b1b4b3" \
-   -color-urgent "#222222, #b1b4b3, #222222, #77003d, #b1b4b3" \
    -kb-row-select "Tab" -kb-row-tab ""
