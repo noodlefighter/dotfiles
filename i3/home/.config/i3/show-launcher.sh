d3be2cf227c40ee55bf97d75404e5291617a9770
noodlefighter <noodlefighter@gmail.com>
2021-05-31 15:09:03 +0800

    i3wm

--- i3/home/.config/i3/show-launcher.sh
+++ i3/home/.config/i3/show-launcher.sh
@@ -19,7 +19,7 @@ if ! [ $? = 0 ]; then
 fi
 
 rofi -dpi 1 -show combi \
-   -combi-modi window,run,drun -width 30 -lines 10 -columns 1 \
+   -combi-modi window,drun,run -width 30 -lines 10 -columns 1 \
    -separator-style none -font "mono 12" -bw 0 \
    -show-icons \
    -kb-row-select "Tab" -kb-row-tab ""
