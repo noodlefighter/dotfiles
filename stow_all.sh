dad727e1bbd0d53b9bd1375898ffa954a5d88a02
noodlefighter <noodlefighter@gmail.com>
2021-06-03 22:52:57 +0800

    private rootfs

--- stow_all.sh
+++ stow_all.sh
@@ -1,20 +1,40 @@
 #!/bin/bash
 
+HOST=$(hostname)
+
 cd "$(dirname "$0")"
+
 
 do_restow() {
 	stow --target=$HOME -R $*
 }
 
+echo restow home...
 do_restow home
+
+echo restow ibus...
 do_restow ibus
-do_restow private
+
+echo restow private/home ...
+stow --target=$HOME -R --dir=private home/
+
+echo restow i3/home ...
 stow --target=$HOME -R --dir=i3 home/
 
+echo write hosts file ...
 cd ./rootfs/hosts/
 sudo $HOME/bin/replace-file-segment /etc/hosts dotfiles-hosts ./hosts '#'
-
-if [[ -f "./$(hostname)" ]]; then
+if [[ -e "./$HOST" ]]; then
 	sudo $HOME/bin/replace-file-segment /etc/hosts dotfiles-hosts-for-this-host "./$(hostname)" '#'
 fi
+cd - > /dev/null
 
+echo copy private/rootfs ...
+cd ./private/rootfs/
+if [[ -d "./$HOST" ]]; then
+  cd $HOST
+  sudo cp -vrf $PWD/* /
+fi
+cd - > /dev/null
+
+echo DONE!!!
